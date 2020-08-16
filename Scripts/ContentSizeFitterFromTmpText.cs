f5a230b922342328ab718f898bc18a70ce3a3ea8
baba_s <shotababa1206@gmail.com>
2020-08-16 20:36:32 +0900

    first commit

---
+++ Scripts/ContentSizeFitterFromTmpText.cs
@@ -0,0 +1,52 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kogane
+{
+	[DisallowMultipleComponent]
+	public sealed class ContentSizeFitterFromTmpText : MonoBehaviour
+	{
+		[SerializeField] private RectTransform     m_self              = default;
+		[SerializeField] private TMP_Text          m_tmpText           = default;
+		[SerializeField] private ContentSizeFitter m_contentSizeFitter = default;
+		[SerializeField] private Vector2           m_offset            = default;
+
+		private void Reset()
+		{
+			m_self              = GetComponent<RectTransform>();
+			m_tmpText           = GetComponentInChildren<TMP_Text>();
+			m_contentSizeFitter = GetComponentInChildren<ContentSizeFitter>();
+		}
+
+		public void Fit()
+		{
+			Fit
+			(
+				target: m_self,
+				tmpText: m_tmpText,
+				contentSizeFitter: m_contentSizeFitter,
+				offset: m_offset
+			);
+		}
+
+		public static void Fit
+		(
+			RectTransform     target,
+			TMP_Text          tmpText,
+			ContentSizeFitter contentSizeFitter,
+			Vector2           offset
+		)
+		{
+			tmpText.ForceMeshUpdate();
+			contentSizeFitter.SetLayoutHorizontal();
+			contentSizeFitter.SetLayoutVertical();
+
+			var sizeDelta = new Vector2( tmpText.preferredWidth, tmpText.preferredHeight );
+
+			sizeDelta += offset;
+
+			target.sizeDelta = sizeDelta;
+		}
+	}
+}
